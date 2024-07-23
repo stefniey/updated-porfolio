7272a548f296b3b4fadf1c162bee89c87977ebcb
stefniey <stephaniealor7@gmail.com>
2024-07-23 13:51:16 -0700

    Your commit message

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-
-root.render(
+ReactDOM.render(
   <BrowserRouter>
-  <App />
+    <App />
   </BrowserRouter>,
+  document.getElementById('root')
 );
+
+
+
+
+// import React from 'react'
+// import App from './App'
+// import './index.css'
+// import { BrowserRouter } from 'react-router-dom';
+//  import { createRoot } from 'react-dom/client';
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+
+// root.render(
+//   <BrowserRouter>
+//   <App />
+//   </BrowserRouter>,
+// );
+
 
 
 
